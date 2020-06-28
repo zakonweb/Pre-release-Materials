e927a32cb2dbf73270b23ae491c55525e04fb6c6
Zafar Ali Khan <zakonweb@gmail.com>
2020-06-29 00:46:27 +0500

    PRM - CS9608 - P2 P4 - Oct/Nov - 2020 - All Variants

---
+++ Oct-2020/PRM - AL/PRM - A2 - P4/Task2 - P4/students.pl
@@ -0,0 +1,24 @@
+/*
+A2 Computer Science 9608, Pre Release Material (All Variants) -- Oct/Nov 2020")
+PROLOG code by Zak; http://www.zakonweb.com")
+*/
+person(luke).
+person(alice).
+person(taylor).
+person(nadia).
+person(mehrdad).
+person(nigel).
+colour(blue).
+colour(red).
+colour(green).
+colour(yellow).
+colour(pink).
+colour(orange).
+colour(purple).
+colour(black).
+likes_colour(alice, yellow).
+likes_colour(alice, blue).
+likes_colour(nadia, red).
+dislikes_colour(taylor, red).
+dislikes_colour(nadia, green).
+dislikes_colour(mehrdad, pink).
